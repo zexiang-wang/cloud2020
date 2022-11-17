ac609ef23ddbd4793f96782a5fe735f8575f5685
logan <17751758772@163.com>
2022-11-17 13:33:46 +0800

    gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/springcloud/cloudgatewaygateway9527/CloudGatewayGateway9527Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.springcloud.cloudgatewaygateway9527;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGatewayGateway9527Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGatewayGateway9527Application.class, args);
+    }
+
+}
