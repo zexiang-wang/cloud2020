a692c5e8625f65cae4492043465b571cb133c0a5
logan <17751758772@163.com>
2022-11-22 14:50:51 +0800

    gateway

--- cloud-gateway-gateway9527/src/test/java/com/atguigu/springcloud/CloudGatewayGateway9527ApplicationTests.java
+++ cloud-gateway-gateway9527/src/test/java/com/atguigu/springcloud/CloudGatewayGateway9527ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.atguigu.springcloud.cloudgatewaygateway9527;
+package com.atguigu.springcloud;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
